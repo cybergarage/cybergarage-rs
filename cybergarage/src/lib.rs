69157f7e4ed6e6ea4e1f39d7f7db330b4f131480
Satoshi Konno <skonno@cybergarage.org>
2022-12-02 12:56:27 +0900

    Update lib.rs

--- cybergarage/src/lib.rs
+++ cybergarage/src/lib.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//! A Rust common cargo package for cybergarage.org crates.
+
 /// Encoding function module.
 pub mod encoding;
 /// Logger function module.
