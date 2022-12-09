ffe787062cfd8c764ee6139d608cde00b36038fd
Satoshi Konno <skonno@cybergarage.org>
2022-12-10 02:20:46 +0900

    Add func!() macro

---
+++ cybergarage/src/macros.rs
@@ -0,0 +1,26 @@
+// Copyright (C) 2022 Satoshi Konno All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/// func! retuns the current function name.
+#[macro_export]
+macro_rules! func {
+    () => {{
+        fn f() {}
+        fn type_name_of<T>(_: T) -> &'static str {
+            std::any::type_name::<T>()
+        }
+        let name = type_name_of(f);
+        &name[..name.len() - 3 /* ""::f" */]
+    }}
+}
