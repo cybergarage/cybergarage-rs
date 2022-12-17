17829c508cb5a8daeb92c7a118fdeef0c4fce109
Satoshi Konno <skonno@cybergarage.org>
2022-12-17 19:05:43 +0900

    Remove unused constants

--- cybergarage/src/net/default.rs
+++ cybergarage/src/net/default.rs
@@ -12,9 +12,4 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::net::{Ipv4Addr, Ipv6Addr};
-
-pub const MULTICAST_V4_ADDRESS: Ipv4Addr = Ipv4Addr::new(224, 0, 23, 0);
-pub const MULTICAST_V6_ADDRESS: Ipv6Addr = Ipv6Addr::new(0xff, 0x02, 0, 0, 0, 0, 0, 1);
-pub const PORT: u16 = 3610;
 pub const MAX_PACKET_SIZE: usize = 1024;
