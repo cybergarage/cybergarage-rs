cc3cbf4113c132ae59ea546331a5ea420cf41fe6
Satoshi Konno <skonno@cybergarage.org>
2024-04-04 01:05:14 +0900

    Update test::decode_from()

--- cybergarage/src/log/hexdump/decoder_test.rs
+++ cybergarage/src/log/hexdump/decoder_test.rs
@@ -13,26 +13,38 @@
 // limitations under the License.
 
 #[cfg(test)]
-mod tests {
+mod test {
     use crate::log::hexdump::*;
 
     #[test]
     fn decode_from() {
-        let log = include_str!("log/hexdump01.log");
-        let hexdump_bytes = Decoder::from_log(log);
-        assert!(hexdump_bytes.is_ok());
-        if hexdump_bytes.is_err() {
-            return;
+        struct Test {
+            log: &'static str,
+            expected_bytes: &'static [u8],
         }
 
-        let expected_bytes = include_bytes!("log/hexdump01.bin");
-        let hexdump_bytes = hexdump_bytes.unwrap();
-        assert_eq!(expected_bytes.len(), hexdump_bytes.len());
-        if expected_bytes.len() != hexdump_bytes.len() {
-            return;
-        }
-        for n in 0..expected_bytes.len() {
-            assert_eq!(expected_bytes[n], hexdump_bytes[n]);
+        let tests = vec![
+            Test {
+                log: include_str!("log/hexdump01.log"),
+                expected_bytes: include_bytes!("log/hexdump01.bin"),
+            },
+        ];
+
+        for test in tests {
+            let hexdump_bytes = Decoder::from_log(test.log);
+            assert!(hexdump_bytes.is_ok());
+            if hexdump_bytes.is_err() {
+                return;
+            }
+
+            let hexdump_bytes = hexdump_bytes.unwrap();
+            assert_eq!(test.expected_bytes.len(), hexdump_bytes.len());
+            if test.expected_bytes.len() != hexdump_bytes.len() {
+                return;
+            }
+            for n in 0..test.expected_bytes.len() {
+                assert_eq!(test.expected_bytes[n], hexdump_bytes[n]);
+            }
         }
     }
 }
