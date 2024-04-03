08f3f4d1d1c8136bb9e920c9a6c2a9d1bbd174bc
Satoshi Konno <skonno@cybergarage.org>
2024-04-04 01:14:47 +0900

    Update mod.rs

--- cybergarage/src/log/hexdump/mod.rs
+++ cybergarage/src/log/hexdump/mod.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-pub use self::decoder::Decoder;
+pub use self::decoder::*;
 pub use self::error::*;
 
 mod decoder;
