37e02392ba78d3428f57f76b8d3f1cfeb0de9fdf
Satoshi Konno <skonno@cybergarage.org>
2024-04-03 12:16:35 +0900

    Update Decoder::from_bytes()

--- cybergarage/src/log/hexdump/decoder.rs
+++ cybergarage/src/log/hexdump/decoder.rs
@@ -12,10 +12,37 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::io::Error;
+
 pub struct Decoder {}
 
 impl Decoder {
-    pub fn from_bytes(bytes: &mut [u8]) {}
-    pub fn from_str(s: &str) {}
+    pub fn from_bytes(bytes: &mut [u8]) -> Result<Vec<u8>, Error> {
+        Decoder::from_str(std::str::from_utf8(bytes).unwrap())
+    }
+
+    pub fn from_str(s: &str) -> Result<Vec<u8>, Error> {
+        // if len(src) == 0 {
+        //     return []byte{}, nil
+        // }
+        // splitHexes := strings.Split(src, " ")
+        // lineHexes := splitHexes[1 : hexdumpTwoColumnBytes+3]
+        // var bytes []byte
+        // for _, s := range lineHexes {
+        //     if len(s) == 0 {
+        //         continue
+        //     }
+        //     hexByte, err := hex.DecodeString(s)
+        //     if err != nil {
+        //         return bytes, err
+        //     }
+        //     bytes = append(bytes, hexByte...)
+        // }
+        // return bytes, nil
+        if s.len() == 0 {
+            return Ok(vec![]);
+        }
+        Ok(vec![])
+    }
     pub fn from_lines(lines: &Vec<&str>) {}
 }
