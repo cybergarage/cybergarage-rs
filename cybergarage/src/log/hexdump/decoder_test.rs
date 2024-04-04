d2a55584d2973f217f7fd8b32c61844a8ebcf7bb
Satoshi Konno <skonno@cybergarage.org>
2024-04-04 09:05:32 +0900

    Update tests::decode_from()

--- cybergarage/src/log/hexdump/decoder_test.rs
+++ cybergarage/src/log/hexdump/decoder_test.rs
@@ -12,34 +12,37 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use crate::log::hexdump::*;
+#[cfg(test)]
+mod tests {
+    use crate::log::hexdump::*;
 
-#[test]
-fn decode_from() {
-    struct Test {
-        log: &'static str,
-        expected_bytes: &'static [u8],
-    }
-
-    let tests = vec![Test {
-        log: include_str!("log/hexdump01.log"),
-        expected_bytes: include_bytes!("log/hexdump01.bin"),
-    }];
-
-    for test in tests {
-        let hexdump_bytes = Decoder::from_log(test.log);
-        assert!(hexdump_bytes.is_ok());
-        if hexdump_bytes.is_err() {
-            return;
+    #[test]
+    fn decode_from() {
+        struct Test {
+            log: &'static str,
+            expected_bytes: &'static [u8],
         }
 
-        let hexdump_bytes = hexdump_bytes.unwrap();
-        assert_eq!(test.expected_bytes.len(), hexdump_bytes.len());
-        if test.expected_bytes.len() != hexdump_bytes.len() {
-            return;
-        }
-        for n in 0..test.expected_bytes.len() {
-            assert_eq!(test.expected_bytes[n], hexdump_bytes[n]);
+        let tests = vec![Test {
+            log: include_str!("log/hexdump01.log"),
+            expected_bytes: include_bytes!("log/hexdump01.bin"),
+        }];
+
+        for test in tests {
+            let hexdump_bytes = Decoder::from_log(test.log);
+            assert!(hexdump_bytes.is_ok());
+            if hexdump_bytes.is_err() {
+                return;
+            }
+
+            let hexdump_bytes = hexdump_bytes.unwrap();
+            assert_eq!(test.expected_bytes.len(), hexdump_bytes.len());
+            if test.expected_bytes.len() != hexdump_bytes.len() {
+                return;
+            }
+            for n in 0..test.expected_bytes.len() {
+                assert_eq!(test.expected_bytes[n], hexdump_bytes[n]);
+            }
         }
     }
 }
